778a2bbe8d0cef04b4eb9205adeb976083d32e7b
yeyintkoko <yeyintkoko15@gmail.com>
2017-10-06 13:28:33 +0630

    added data model

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,39 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by AGD on 10/6/17.
+//  Copyright © 2017 AGD. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
