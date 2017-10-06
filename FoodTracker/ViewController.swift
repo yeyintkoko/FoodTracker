5a227111bd47de4c55ff3f7f13a53d477fbe8ec6
yeyintkoko <yeyintkoko15@gmail.com>
2017-10-06 12:45:28 +0630

    implemented rating starts

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -70,15 +71,5 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
     }
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = getMaskText(label:"Ko Narya", id: 45)
-        print("Hey you go!!")
-    }
-    
-    func getMaskText(label: String, id: Int) -> String {
-        return label + "-" + String(id)
-    }
-
-
 }
 
